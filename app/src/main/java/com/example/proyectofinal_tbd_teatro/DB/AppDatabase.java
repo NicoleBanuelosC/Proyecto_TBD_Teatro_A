dfef0e98804c2594e62d1ef61e6fd0f63987798c
Nicole Bañuelos <nicole.bc@tecjerez.edu.mx>
2025-12-10 08:10:55 -0600

    comentar codigo para no perderme

--- app/src/main/java/com/example/proyectofinal_tbd_teatro/DB/AppDatabase.java
+++ app/src/main/java/com/example/proyectofinal_tbd_teatro/DB/AppDatabase.java
@@ -16,21 +16,24 @@ public abstract class AppDatabase extends RoomDatabase {
     public abstract com.example.proyectofinal_tbd_teatro.obra.ObraDao obraDao();
     public abstract com.example.proyectofinal_tbd_teatro.produccion.ProduccionDao produccionDao();
 
-    private static volatile AppDatabase INSTANCE;
+    //SINGLETON
+    private static volatile AppDatabase INSTANCE; //variable estatica unica
 
     public static AppDatabase getDatabase(final Context context) {
-        if (INSTANCE == null) {
-            synchronized (AppDatabase.class) {
+        if (INSTANCE == null) { //verifica si existe
+            synchronized (AppDatabase.class) { //eviat problemas con los hilos
                 if (INSTANCE == null) {
-                    INSTANCE = Room.databaseBuilder(
+                    INSTANCE = Room.databaseBuilder( //crea la instancia SOLO UNA VEZ
                                     context.getApplicationContext(),
                                     AppDatabase.class,
                                     "teatro_pleasantville_db"
                             )
                             .build();
-                }
-            }
-        }
+                }//if
+            }//synchronizad
+        }//if afuera
+
         return INSTANCE;
-    }
-}
+    }//public static
+
+}//public class
